c27b17ea5aea170da4cdce81695bbc165bce8e4f
bhavesh <bhaveshgupta41@gmail.com>
2023-07-09 16:43:17 +0530

    project complte

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 var host = process.env.HOST || '0.0.0.0';
 const mysql = require('mysql');
 app.use(cors());
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'SmileHappy',
     database: 'booking',
-})
+}) 
 
-app.get("/", (req, res) => {
-    console.log("wokring");
-    con.query('SELECT * FROM usertable;', (err, result)=>{
-        if(err){
-            console.log("error");
-            res.send(err);
-        }
-        console.log("success");
-        res.send(result);
-    })
-})
+//get data whose status is 1, that is booked
+app.get('/data', (req, res) => {
+  const query = 'SELECT * FROM usertable WHERE status = 1';
 
-con.connect((err)=> {
-   if(err){
-    console.log("connect erro");
-   }
-   else{
-    console.log("connected");
-   }
-})
-
-app.put('/:ids', (req, res) => {
-    const ids = req.params.ids.split(',');
-    const status = req.body.status;
-  
-    ids.forEach(id => {
-      con.query(
-        'UPDATE usertable SET status = ? WHERE id = ?',
-        [status, id],
-        (err, result) => {
-          if (err) {
-            console.error(err);
-            res.sendStatus(500);
-          } else {
-            console.log(`Data updated for ID ${id}:`, result);
-          }
-        }
-      );
-    });
-    res.sendStatus(200);
+  con.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+     const seatValues = results.map((row) => row.seat);
+      res.status(200).json(seatValues);
+    }
   });
+});
 
+
+app.put('/seats', (req, res) => {
+    console.log(req.body)
+    const { seatIds, status } = req.body;
+  
+    // Convert seatIds to an array if it's a single ID
+    const seatIdArray = Array.isArray(seatIds) ? seatIds : [seatIds];
+  
+    // Prepare the SQL query
+    const updateQuery = `UPDATE usertable SET status = ? WHERE seat IN (?)`;
+    const values = [status, seatIdArray];
+  
+    // Execute the query
+    con.query(updateQuery, values, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while updating seat status.' });
+      } else {
+        res.json({ seatIdArray });
+      }
+    });
+});
+  
 app.listen(port,host, (err)=>{
     if(err){
         console.log("custom 2 err");
